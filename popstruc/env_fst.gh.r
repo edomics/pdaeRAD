44f1a6a4db30de3b72ec097503e95da64a0a4442
Ed Smith <85354630+edomics@users.noreply.github.com>
2021-11-10 18:35:11 -0500

    Update env_fst.gh.r

--- popstruc/env_fst.gh.r
+++ popstruc/env_fst.gh.r
@@ -71,58 +71,6 @@ AIC.table[,3:ncol(AIC.table)] <- round(AIC.table[,3:ncol(AIC.table)], 4)
 AIC.table
 write.csv(AIC.table, file = "AIC.table_lm_bst_ALL_models.csv") # MAIN RESULTS
 
-#Now we ran a signficance test based on permutations. This function was adapted from the MMRR function developed by Wang (Wang 2013). 
-# Define matrices 
-# best model was model 3
-Y = cbind(scale(bstdata.fst$fst))
-X = cbind((bstdata.fst$barrier),scale(bstdata.fst$geod),scale(bstdata.fst$envd))
-colnames(X)=c("barrier","geod","envd")
-
-#define function MMRR that compute regression coefficients and test statistics
-MMRR<-function(Y,X,nperm=999){  
-  nrowsY<-nrow(Y)
-  y<-(Y)
-  if(is.null(colnames(X)))colnames(X)<-paste("X",1:length(X),sep="")
-  Xmats<-(X)
-  fit<-lm(y~factor(Xmats[,1])+Xmats[,3]+Xmats[,2])
-  coeffs<-fit$coefficients
-  summ<-summary(fit)
-  r.squared<-summ$r.squared
-  tstat<-summ$coefficients[,"t value"]
-  Fstat<-summ$fstatistic[1]
-  tprob<-rep(1,length(tstat))
-  Fprob<-1
-  
-  #perform permutations
-  for(i in 1:nperm){
-    rand<-sample(1:nrowsY)
-    Yperm<-Y[rand]
-    yperm<-(Yperm)
-    fit<-lm(yperm~factor(Xmats[,1])+Xmats[,3]+Xmats[,2])
-    summ<-summary(fit)
-    Fprob<-Fprob+as.numeric(summ$fstatistic[1]>=Fstat)
-    tprob<-tprob+as.numeric(abs(summ$coefficients[,"t value"])>=abs(tstat))
-  }
-  
-  #return values
-  tp<-tprob/(nperm+1)
-  Fp<-Fprob/(nperm+1)
-  names(r.squared)<-"r.squared"
-  names(coeffs)<-c("Intercept",colnames(X))
-  names(tstat)<-paste(c("Intercept",colnames(X)),"(t)",sep="")
-  names(tp)<-paste(c("Intercept",colnames(X)),"(p)",sep="")
-  names(Fstat)<-"F-statistic"
-  names(Fp)<-"F p-value"
-  return(list(r.squared=r.squared,
-              coefficients=coeffs,
-              tstatistic=tstat,
-              tpvalue=tp,
-              Fstatistic=Fstat,
-              Fpvalue=Fp))
-}
-#run the function with 9999 permutations
-MMRR (Y,X,nperm=9999)
-
 #Plotting figures for entire dataset
 #with CIs
 ggplot(bstdata.fst,aes(y=scale(fst),x=scale(envd)+scale(geod),colour=factor(barrier))) + geom_point() +scale_color_manual(values=c("orange","blue"))+stat_smooth(data=bstdata.fst,method="lm",se=T)+theme_bw()+guides(color=F) +ylab("Genetic Distance")+xlab("Geo + Env Distance")+theme(axis.text = element_text(size=14), axis.title = element_text(size=20))
